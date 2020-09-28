533a6955a7efc728a6a22a8c275baa5f41c706c2
Clarence Brathwaite <Clarence.Brathwaite@ey.com>
2020-09-28 09:37:19 -0400

    Add project files.

---
+++ UAT_Exception_Handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace UAT_Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. UAT Class Exception Handling Project!");
+
+            try
+            {
+                using (StreamReader sw = new StreamReader("us-500.csv"))
+                {
+                    string buffer = string.Empty;
+                    while ((buffer = sw.ReadLine()) != null)
+                    {
+                        Console.WriteLine(buffer);
+                    }
+                }
+            }
+            catch(Exception excp)
+            {
+                Console.WriteLine("error : " + excp.Message);
+            }
+        }
+    }
+}
